7a39ea47b8f02f46a938752078f7f0ec7cff68b1
SzymKrom <szymonkromiszewski@gmail,com>
2023-06-05 19:55:29 +0200

    dodalismy odejmowanie do kalkulatora

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calculator {
+
+    public double add(double x, double y) {
+        return x + y;
+    }
+    public double sub(double x, double y) {
+        return x - y;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(4, 7));
+        System.out.println(calculator.sub(0.5, 5.5));
+    }
+}
