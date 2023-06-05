09406839c5a0926667d9e852d21c1e69f7e515db
SzymKrom <szymonkromiszewski@gmail,com>
2023-06-05 20:37:09 +0200

    utworzenie banku wplaty

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.add(4, 7));
-        System.out.println(calculator.sub(0.5, 5.5));
+        Bank bank = new Bank();
+        bank.depozyt(400);
+        System.out.println(bank.depozyt(500));
+
     }
 }
